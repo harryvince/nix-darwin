0bdc172d41d74dcb6c180cdd35f3773780b5de7d
Harry Vince <harryavince@gmail.com>
2024-06-04 22:50:34 +0100

    feat: neovim aliases and manage zshrc under nix

--- home-manager.nix
+++ home-manager.nix
@@ -33,5 +33,31 @@
         userEmail = "harryavince@gmail.com";
     };
 
-    programs.neovim.enable = true;
+    programs.neovim = { 
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+    };
+
+    programs.zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        enableCompletion = true;
+
+        oh-my-zsh = {
+            enable = true;
+            plugins = ["git" "ssh-agent" "fnm" "fzf" "z"];
+            theme = "robbyrussell";
+        };
+
+        shellAliases = {
+            wt = "git worktree";
+            lg = "lazygit";
+            pi5="ssh pi5.vince.io";
+            tf="terraform";
+            python="python3";
+            brew-update="brew update && brew upgrade && brew autoremove && brew cleanup -s && brew cleanup --prune=all";
+            ldo="lazydocker";
+        };
+    };
 }
